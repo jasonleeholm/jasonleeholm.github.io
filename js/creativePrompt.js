136cbb610266b941993e8802c48f0a2da09d6d37
jasonleeholm <7604613+jasonleeholm@users.noreply.github.com>
2026-07-07 10:40:50 -0400

    Refactor creative prompt message formatting

--- js/creativePrompt.js
+++ js/creativePrompt.js
@@ -50,7 +50,9 @@ function generatePrompt() {
     const noun = randomItem(nouns);
 
     document.getElementById("creativePrompt").innerHTML =
-        `Come up with an idea using <strong>${articleFor(adjective)} ${adjective} ${noun}</strong> as the subject of inspiration.<br/>
+        `Come up with an idea using<br/>
+        <strong>${articleFor(adjective)} ${adjective} ${noun}</strong><br/>
+        as the subject of inspiration.<br/>
         Interpret the prompt however you choose.<br/>
         Use any creative domain, medium, method, style, technique, or tool you want!`;
 
